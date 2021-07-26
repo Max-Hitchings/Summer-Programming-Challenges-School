babe3359487dba2a9da41da299f85e5c03143456
gk41 <max.hitchings@gmail.com>
2021-07-26 16:22:13 +0100

    Working TreasureHunt game

--- src/games/TreasureHunt/TreasureHunt.js
+++ src/games/TreasureHunt/TreasureHunt.js
@@ -15,7 +15,15 @@ export default function TreasureHunt() {
   };
 
   const CheckForGold = (row, column) => {
-    console.log(row, column, Gold);
+    if (Gold[0] === row && Gold[1] === column) {
+      document.getElementById(`row-${row},column-${column}`).className = `${
+        document.getElementById(`row-${row},column-${column}`).className
+      } Board-Item-Corect`;
+    } else {
+      document.getElementById(`row-${row},column-${column}`).className = `${
+        document.getElementById(`row-${row},column-${column}`).className
+      } Board-Item-Incorect`;
+    }
   };
 
   const ColumnGen = (row) => {
@@ -27,6 +35,7 @@ export default function TreasureHunt() {
           className={classes.BoardItem}
           onClick={() => CheckForGold(row, column)}
           key={`row-${row},column-${column}`}
+          id={`row-${row},column-${column}`}
         ></Grid>
       );
     }
@@ -46,9 +55,13 @@ export default function TreasureHunt() {
   };
 
   return (
-    <div className={classes.TreasureHunt}>
-      <div className={classes.BoardContainer}>{BoardGen()}</div>
-    </div>
+    <>
+      <div className={classes.TreasureHunt}>
+        <div className={classes.BoardContainer}>{BoardGen()}</div>
+        <div className={classes.BoardControls}>TEST</div>
+      </div>
+      <div className={classes.BoardControls}>hi</div>
+    </>
   );
 }
 
@@ -65,7 +78,7 @@ const useStyles = makeStyles({
     height: "500px",
     alignSelf: "center",
     marginLeft: "auto",
-    marginRight: "auto",
+    marginRight: "0",
   },
   BoardRow: {
     height: "10%",
@@ -74,5 +87,13 @@ const useStyles = makeStyles({
     width: "10%",
     height: "100%",
     border: "1px solid",
+  },
+  BoardControls: {
+    width: "150px",
+    height: "150px",
+    backgroundColor: "orange",
+    alignSelf: "center",
+    marginLeft: "50px",
+    marginRight: "auto",
   },
 });
