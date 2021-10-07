6352df68fe562cc188bbdeb7f4a90e0927793bde
Max <72921542+Max-Hitchings@users.noreply.github.com>
2021-10-07 12:18:43 +0100

    Update RPS.py

--- RPS-Python/RPS.py
+++ RPS-Python/RPS.py
@@ -6,17 +6,20 @@ global gameScore
 
 gameScore = {"player": 0, "comp": 0}
 
-f = open('website\RPS-Python\gameRules.json',)
+f = open('./gameRules.json',)
 gameRules = json.load(f)
 
 def RPSLogic(userPick):
     compPick = gameRules["comp"][str(random.randint(1,3))]
 
     if userPick == compPick:
+        print("you DREW that round")
         return 0
     elif userPick == gameRules[compPick]["loses"]:
+        print("YOU WON THAT ROUND")
         return 1
     elif userPick == gameRules[compPick]["beats"]:
+        print("you LOST that round")
         return -1
 
 def takeUserInput():
@@ -28,22 +31,18 @@ def takeUserInput():
             userPick = input("please enter a valid input (rock, paper, scissors)")
 
 def updateScore(RPSResult):
-    print(RPSResult)
     if RPSResult == 1:
         gameScore["player"] = gameScore["player"] + 1
     elif  RPSResult == -1:
         gameScore["comp"] = gameScore["comp"] + 1
     return RPSResult
-
-def resetScore():
-    gameScore = {"player": 0, "comp": 0}
-
-def startGame():
-    while True:
     
+def startGame():
+    global gameScore 
+    while True:
         for x in range(3):
             updateScore(RPSLogic(takeUserInput()))
-        print(gameScore)
+
         if gameScore["player"] == gameScore["comp"]:
             while True:
                 print("Tie breaker")
@@ -56,6 +55,7 @@ def startGame():
         if input("Do you want to play again? (yes/no)") not in gameRules["allowedPicks"]["again"]:
             break
 
-        resetScore()
+        gameScore = {"player": 0, "comp": 0}
+       
 
 startGame()
